a129448f38795b45008dc20effef36cb5ee704b5
Alexander Telecan <alexander@Alexanders-MacBook-Pro.local>
2022-07-15 19:03:23 +0300

    universal links still not opening

--- App.js
+++ App.js
@@ -5,13 +5,26 @@ import { StyleSheet, Text, View } from "react-native";
 import MainNavigator from "./src/navigation/mainNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import config from "./src/deepLinking/linking";
+import { useEffect } from "react";
 
-const prefix = Linking.createURL("/www.nestertest.com");
+const prefix = Linking.createURL("/");
 export default function App() {
   const linking = {
     prefixes: [prefix],
     config,
   };
+  const checkAppSource = async () => {
+    const source = await Linking.getInitialURL();
+    if (source) {
+      console.log("User came from a link");
+    } else {
+      console.log("User came from the app");
+    }
+  };
+
+  useEffect(() => {
+    checkAppSource();
+  }, []);
   return (
     <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
       <MainNavigator />
